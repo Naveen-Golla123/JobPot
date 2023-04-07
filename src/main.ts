b7d5615d789d40e28b7ecc3a14e118dd6ec6f542
Naveen Golla-123 <naveengolla123@gmail.com>
2023-04-06 23:11:40 -0700

    Import corrected

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
-  app.use(cors());
+  const CORS_OPTIONS = {
+    origin: ['*'], // or '*' or whatever is required
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    exposedHeaders: 'Authorization',
+    credentials: true,
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
+  };
+
+  const adapter = new FastifyAdapter();
+  adapter.enableCors(CORS_OPTIONS)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
+
+  await app.listen(3000);
 }
 bootstrap();
